a5c7494f3f852d2fabb987cc23941472eac0acbf
Sam Mckay <sammckay12@gmail.com>
2017-04-05 09:40:49 +0100

    created the container and input shells for the url input

---
+++ src/shortenerContainer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import UrlInput from './UrlInput'
+
+export default class ShortnerView extends Component {
+
+
+};
